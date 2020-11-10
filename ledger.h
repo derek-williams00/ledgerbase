635ba92bc209bc1966fd2a861fb7763c83eb8e54
derek-williams00 <derekOfficial00@gmail.com>
2020-11-09 22:40:00 -0800

    worked on ledger.h

--- ledger.h
+++ ledger.h
@@ -1,6 +1,15 @@
+/*
+ *
+ */
+
 #ifndef __LEDGER_H
 #define __LEDGER_H
 
+
+
+/*********************
+ *     STRUCTURES    *
+ *********************/
 
 /*
  * Non-transportable
@@ -36,51 +45,73 @@ struct ledger_state;
 struct ledger_charter;
 
 
-/* NOTES:
- *
- * basis ( charter , initial_state )
- * ledger ( basis )
- *
- *
- *
- */
 
 
 
+/*********************
+ *     FUNCTIONS     *
+ *********************/
 
+
+/**/
 struct ledger_charter* ledger_charter_create(
   int n_ins,
   const char* ins,
-  void (charter_builder*)(struct ledger_charter* p_charter, int n_ins, const char* ins)
+
+  /* TODO: What are the constraints on this function */
+  void (*builder)(struct ledger_charter* p_charter, int n_ins, const char* ins)
 );
 
-struct ledger_basis* ledger_basis_create(
+
+
+/**/
+struct ledger* ledger_create(
   const struct ledger_charter* p_charter,
-  const struct ledger_state* p_initial_state,
+  int n_ins,
+  const char* ins,
+
+  /* TODO: What are the constraints on this function */
+  void (*builder)(struct ledger* p_ledger, int n_ins, const char* ins)
 );
 
-
-struct ledger* ledger_create(const struct ledger_basis* p_basis);
+/**/
 void ledger_free(struct ledger* p_ledger);
 
+const struct ledger_charter* ledger_get_original_charter(const struct ledger* p_ledger);
+const struct ledger_state* ledger_get_initial_state(const struct ledger* p_ledger);
+const struct ledger_basis* ledger_get_basis(const struct ledger* p_ledger);
+
+/**/
 struct ledger_id* ledger_get_id(const struct ledger_basis* p_basis);
 
 
-
-
-
-/*
- * Get chapter from ledger.
+/* Get chapter from ledger.
  *
  * Can return NULL
  */
-const struct ledger_chapter* ledger_get_chapter(int n);
+const struct ledger_chapter* ledger_get_chapter(const struct ledger* p_ledger, int n);
 
 
+/* Get state from ledger.
+ *
+ * Can return NULL
+ */
+const struct ledger_chapter* ledger_get_state(const struct ledger* p_ledger, int n);
+
+
+/* Determine if a chapter is valid.
+ *
+ * "valid" does not mean "binding"
+ */
+int ledger_chapter_is_valid(const struct ledger_chapter* p_chapter); 
+
+/**/
 struct ledger_id* ledger_id_create(const struct ledger_basis* p_basis);
 
+/**/
 void ledger_id_free(struct ledger_id* p_ledger_id);
 
+/**/
 int equal_ledger_ids(const struct ledger_id* p_a, const struct ledger_id* p_b);
 
 
@@ -103,14 +134,18 @@ const struct ledger_chapter* chapter_get_prev(const struct ledger_chapter* p_cha
  */
 int chapter_get_span(const struct ledger_chapter* p_chapter);
 
+/**/
 struct ledger_state* ledger_initial_state(const struct ledger* p_ledger);
 
+/**/
 struct ledger_state* chapter_suppose(const struct ledger_chapter* p_chapter);
 
 
 
+/**/
 struct ledger_state* ledger_initial_state(const struct ledger_basis* p_basis);
 
+/**/
 struct ledger_charter* basis_get_charter(const struct ledger_basis* p_basis);
 
 
